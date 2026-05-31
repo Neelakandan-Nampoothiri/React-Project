8bf05fb002a95ce7b4cdf1cc239aadc0b5194b63
Neelakandan Nampoothiri N <neelakandannampoothirin@gmail.com>
2026-05-31 12:23:53 +0530

    Updated Navbar

---
+++ proj/src/components/OurWork.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import assets from '../assets/assets'
+import Title from './Title'
+
+const OurWork = () => {
+
+    const workdata = [
+        {
+            title: 'Mobile app Marketing',
+            description: 'we turn bold ideas into powerful digital solutions that connect,  engage..',
+            image : assets.work_mobile_app
+
+        },
+        {
+            title: 'Dashboard Management',
+            description: 'we help you execute your plan and deliver results.',
+            image : assets.work_dashboard_management
+
+        },
+        {
+            title: 'Fitness app promotion',
+            description: 'we help you create a marketing strategy that drives result.',
+            image : assets.work_fitness_app
+
+        },
+
+    ]
+
+  return (
+    <>
+        <div id='our-work' className='flex flex-col items-center gap-7 px-4
+        sm-px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white '>
+
+            <Title title='Our latest work' desc='Browse our portfolio of innovative digital projects that showcase creativity,performance, and results.'/>
+            <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl'>
+
+                {
+                    workdata.map((work,index)=>(
+                        <div key={index} className='hover:scale-102 duration-500 transition-all cursor-pointer'>
+                                    <img src={work.image} alt='' className='w-full rounded-xl'/>
+                                    <h3 className='mt-3 mb-2 text-lg font-semibold'>{work.title}</h3>
+                                    <p className='text-sm opacity-60 w-5/6'>{work.description}</p>
+                        </div>
+                    ))
+                }
+            </div>
+            
+        </div>
+    </>
+  )
+}
+
+export default OurWork
