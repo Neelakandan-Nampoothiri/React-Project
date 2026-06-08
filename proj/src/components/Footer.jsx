63203170f1f84fa6d3397b9c87cfc9e0b1593e14
Neelakandan Nampoothiri N <neelakandannampoothirin@gmail.com>
2026-06-08 11:08:45 +0530

    added animation

--- proj/src/components/Footer.jsx
+++ proj/src/components/Footer.jsx
@@ -41,8 +41,8 @@ const Footer = ({theme}) => {
             </motion.div>
             <motion.div 
             
-                initial={{opacity:0 ,x:30}}
-                whileInView={{opacity:1, x:0}}
+                initial={{opacity:0 ,y:30}}
+                whileInView={{opacity:1, y:0}}
                 transition={{duration:0.6,delay:0.3}}
                 viewport={{once:true}}
             
